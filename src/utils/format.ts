21b02fd2018a48aaa1be8aa80635d13c8a9f3515
Eduardo Vergara <eduver_san@hotmail.com>
2026-05-20 10:29:54 +0100

    feat: Add COP currency format function

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -6,6 +6,15 @@ export const formatCurrency = (amount: number): string => {
   }).format(amount);
 };
 
+export const formatCurrencyCOP = (amount: number): string => {
+  return new Intl.NumberFormat('es-CO', {
+    style: 'currency',
+    currency: 'COP',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(amount).replace(/\s/g, '');
+};
+
 export const formatPercentage = (value: number): string => {
   return `${value.toFixed(1)}%`;
 };
